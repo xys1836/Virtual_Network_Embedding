ac26096e7cb08e787bec0e00dba1b7051e899e1f
Yansen Xu <xys8804@gmail.com>
2016-08-23 23:13:02 +0900

    construct graph function completed

--- Graph_Generator.py
+++ Graph_Generator.py
@@ -1,5 +1,7 @@
 import os
 import subprocess
+import random
+import networkx as nx
 
 def create_script_file(n,g,p,s,name = None):
   """Create Script for itm
@@ -17,16 +19,49 @@ def create_script_file(n,g,p,s,name = None):
   l1 = 'geo' + ' ' + '1' + ' ' + str(s)
   l2 = str(n) + ' ' + str(g) + ' ' + '3' + ' ' + str(p)
   if name:
-    file_path = './graph_set/' + name + '.gb'
+    file_path = './graph_set/' + name 
   else:
-    file_path = './graph_set/' + 'tmp.gb'
-  f = open(file_path, 'wb')
-  f.write(l1 + '\n')
-  f.write(l2 + '\n')
-  f.close()
+    file_path = './graph_set/' + 'no_name_graph'
+  with open(file_path, 'wb') as f:
+    f.write(l1 + '\n')
+    f.write(l2 + '\n')
   return file_path
 
+def execute_itm(file_path):
+  subprocess.call(['itm', file_path])
+  subprocess.call(['sgb2alt', file_path + '-0.gb', file_path + '.alt'])
+  return 0
+
+def generate_graph(n, g, p, s, name = None):
+  G = nx.Graph()
+  for i in range(0, n):
+    G.add_node(i)
+  isEdge = False
+  file_path = create_script_file(n,g,p,s,name)
+  execute_itm(file_path)
+  with open(file_path + '.alt', 'r') as f:
+    for line in f:
+      if isEdge:
+        edge = line.split(' ') 
+        a = int(edge[0])
+        b = int(edge[1])
+        G.add_edge(a,b)
+        print edge,a,b
+        continue
+      if line[0:5] == 'EDGES':# (from-node to-node length a b):':
+        isEdge = True
+  return G
+ 
+def generate_uniform_distribution(a, b):
+  return random.randint(a,b)  
 
 
+"""
 #if '__name__' == '__main__':
-create_script_file(10,10,0.5,0,'Substrate')
+G = generate_graph(100,100,0.11,0,'Substrate')
+print G.number_of_nodes()
+print G.number_of_edges()
+print G.nodes()
+#for i in range(0,100):
+#  print generate_uniform_distribution(1,100)
+"""
